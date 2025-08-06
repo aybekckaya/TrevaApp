acd0154ed18db04cc6ef70035971141c8d7ac69a
Aybek Can Kaya <aybekcankaya@icloud.com>
2025-08-06 13:20:49 +0300

    asdads

--- api.php
+++ api.php
@@ -1,24 +1,5 @@
 <?php
-$host = 'localhost'; // örnek: '127.0.0.1' ya da '123.45.67.89'
-$db   = 'TrevaDB';
-$user = 'aybekcankaya';
-$pass = '1020304050Aa!';
-$charset = 'utf8mb4';
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, // Hataları yakala
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,       // Fetch tarzı
-    PDO::ATTR_EMULATE_PREPARES   => false,                  // Emülasyon kapalı
-];
-
-try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-    echo "✅ MySQL connection successful!";
-} catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
-}
+echo "asdasdasd";
 
 /*
 CREATE DATABASE TrevaDB CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
